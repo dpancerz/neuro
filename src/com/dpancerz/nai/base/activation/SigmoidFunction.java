b4b99b9c30f776abe54549e707d28e0de34a7785
dpancerz <dawid.pancerz@gmail.com>
2018-11-20 00:36:46 +0100

    iniT 2

---
+++ src/com/dpancerz/nai/base/activation/SigmoidFunction.java
@@ -0,0 +1,10 @@
+package com.dpancerz.nai.base.activation;
+
+import com.dpancerz.nai.base.ActivationFunction;
+
+public class SigmoidalFunction implements ActivationFunction {
+    @Override
+    public double applyAsDouble(double operand) {
+        return 1.0 / (1 + );
+    }
+}
